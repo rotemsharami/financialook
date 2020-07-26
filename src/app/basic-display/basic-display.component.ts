b258aaf3ab26f0f34c77e8644a7a9da74e0cfb3c
sharami_rotem <sharami.rotem@gmail.com>
2020-07-26 23:34:13 +0300

    data

--- src/app/basic-display/basic-display.component.ts
+++ src/app/basic-display/basic-display.component.ts
@@ -1,26 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { MetadataService } from '../metadata.service';
+import { Store, select } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { Income } from 'src/incomes/income';
+import { State } from "../reducers/incomes.reducer"
+
 @Component({
   selector: 'app-basic-display',
   templateUrl: './basic-display.component.html',
   styleUrls: ['./basic-display.component.scss']
 })
 export class BasicDisplayComponent implements OnInit {
-  fLdata: any;
-  monthlyIncome: number;
-  constructor(private metadataService: MetadataService) {
-	this.fLdata = this.metadataService.getData();
-	this.monthlyIncome = 0;
+  incomes$: Observable<any[]> = this._store$.pipe(
+    select("incomes")
+  )
+  // fLdata: any;
+  // monthlyIncome: number;
+  constructor(
+    private metadataService: MetadataService,
+    private _store$: Store<State>
+    ) {
+	// this.fLdata = this.metadataService.getData();
+	// this.monthlyIncome = 0;
   }
 
   ngOnInit(): void {
-    
-    if(this.fLdata.income != undefined){
-		if(this.fLdata.income.incomes != undefined){
-			this.fLdata.income.incomes.forEach((obj, index) => {
-				this.monthlyIncome += parseInt(obj.amount)
-			});
-		}
-    }
+    console.log(this.incomes$);
+    // if(this.fLdata.income != undefined){
+    //   if(this.fLdata.income.incomes != undefined){
+    //     this.fLdata.income.incomes.forEach((obj, index) => {
+    //       this.monthlyIncome += parseInt(obj.amount)
+    //     });
+    //   }
+    // }
   }
 }
