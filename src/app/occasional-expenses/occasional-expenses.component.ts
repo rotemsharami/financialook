a80bc41a61b8aed5e48335a11f07d78841bbc104
sharami_rotem <sharami.rotem@gmail.com>
2020-08-29 17:09:08 +0300

    data

--- src/app/occasional-expenses/occasional-expenses.component.ts
+++ src/app/occasional-expenses/occasional-expenses.component.ts
@@ -66,7 +66,7 @@ export class OccasionalExpensesComponent implements OnInit {
 	}
 
 	paymentsChange(i){
-		this.occasionalExpencesFormObj.controls[i].controls.monthlyPayment.setValue(Math.round(parseInt(this.occasionalExpencesFormObj.controls[i].controls.amount.value)/parseInt(this.occasionalExpencesFormObj.controls[i].controls.payments.value)));
+		//this.occasionalExpencesFormObj.controls[i].controls.monthlyPayment.setValue(Math.round(parseInt(this.occasionalExpencesFormObj.controls[i].controls.amount.value)/parseInt(this.occasionalExpencesFormObj.controls[i].controls.payments.value)));
 		//console.log(this.occasionalExpencesFormObj.controls);
 	}
 
@@ -121,10 +121,11 @@ export class OccasionalExpensesComponent implements OnInit {
 				if(this.data.occasionalExpences != undefined){
 					if(this.data.occasionalExpences.length > 0){
 						this.data.occasionalExpences.forEach((obj, index) => {
+							console.log(obj.payDay);
 							const item = this.fb.group({
 								title: obj.title,
 								amount: obj.amount,
-								payDay: obj.payDay,
+								payDay: moment(obj.payDay),
 								methodsofPayment: obj.methodsofPayment,
 								payments: obj.payments,
 								monthlyPayment: obj.monthlyPayment,
